4a5bf01c110dff7644a6e1c8d4e53c2524332771
王天赐 <15565946702@163.com>
2022-05-19 14:18:34 +0800

    Initial commit

---
+++ bash/install.sh
@@ -0,0 +1,7 @@
+pip install torch==1.10.0+cu102 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch-scatter==2.0.9 -f https://pytorch-geometric.com/whl/torch-1.10.0+cu102.html
+pip install torch-cluster==1.5.9 -f https://pytorch-geometric.com/whl/torch-1.10.0+cu102.html
+pip install torch-spline-conv==1.2.1 -f https://pytorch-geometric.com/whl/torch-1.10.0+cu102.html
+pip install torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-1.10.0+cu102.html
+pip install torch-geometric
+pipl install scikit-learn numpy pandas matplotlib seaborn
---
+++ codes/model/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/5/18 14:27
+# @Author  : 王天赐
+# @Email   : 15565946702@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
